60573ccf0b5f101fa27e7e97ffa29f89c296a7de
Hammad Farooq <76645752+Hammadgh@users.noreply.github.com>
2026-04-27 13:26:59 -0700

    Add Head component with Google site verification meta taghead

---
+++ src/app/head.js
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="wA83WgRPghu40_a8j-KC6lIDv29FIIi8EPWHEjHC4-M"
+      />
+    </>
+  );
+}
